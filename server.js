efb34d41a06b4ff614691badc62e35e33410065a
Aaron <trapp.root@gmail.com>
2024-04-08 16:32:15 -0700

    final commit

--- server.js
+++ server.js
@@ -1,72 +1,70 @@
-'use strict';
-import helmet from 'helmet';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+"use strict";
+const helmet = require("helmet");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require("dotenv").config();
 require("./db-connection.js");
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require("./routes/api.js");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner");
 
 const app = express();
 
-app.use(helmet({
-  frameguard: {
-    action: 'deny'
-  },
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      styleSrc: ["'self'", 'aaronwallo.ch'], // Corrected syntax here
-    }
-  },
-  dnsPrefetchControl: false
-}))
+app.use(
+	helmet({
+		frameguard: {
+			action: "deny",
+		},
+		contentSecurityPolicy: {
+			directives: {
+				defaultSrc: ["'self'"],
+				styleSrc: ["'self'", "aaronwallo.ch"],
+			},
+		},
+		dnsPrefetchControl: false,
+	})
+);
 
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get(function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res, next) {
+	res.status(404).type("text").send("Not Found");
 });
 
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        console.log('Tests are not valid:');
-        console.error(e);
-      }
-    }, 3500);
-  }
+	console.log("Your app is listening on port " + listener.address().port);
+	if (process.env.NODE_ENV === "test") {
+		console.log("Running Tests...");
+		setTimeout(function () {
+			try {
+				runner.run();
+			} catch (e) {
+				console.log("Tests are not valid:");
+				console.error(e);
+			}
+		}, 3500);
+	}
 });
 
 module.exports = app; //for testing
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,81 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+let likes = null;
 
+suite("Functional Tests", function () {
+	test("Viewing one stock: GET request to /api/stock-prices/", (done) => {
+		chai
+			.request(server)
+			.get("/api/stock-prices/")
+			.query({ stock: "GOOG", like: "false" })
+			.end((err, res) => {
+				assert.equal(res.status, 200);
+				assert.isObject(res.body);
+				assert.equal(res.body.stockData.stock, "GOOG");
+				done();
+			});
+	});
+	test("Viewing one stock and liking it: GET request to /api/stock-prices/", (done) => {
+		chai
+			.request(server)
+			.get("/api/stock-prices/")
+			.query({ stock: "GOOG", like: "true" })
+			.end((err, res) => {
+				likes = res.body.stockData.likes;
+				assert.equal(res.status, 200);
+				assert.isObject(res.body);
+				assert.equal(res.body.stockData.stock, "GOOG");
+				done();
+			});
+	});
+	test("Viewing the same stock and liking it again: GET request to /api/stock-prices/", (done) => {
+		chai
+			.request(server)
+			.get("/api/stock-prices/")
+			.query({ stock: "GOOG", like: "true" })
+			.end((err, res) => {
+				assert.equal(res.status, 200);
+				assert.isObject(res.body);
+				assert.equal(res.body.stockData.stock, "GOOG");
+				assert.equal(res.body.stockData.likes, likes);
+				done();
+			});
+	});
+	test("Viewing two stocks: GET request to /api/stock-prices/", (done) => {
+		chai
+			.request(server)
+			.get("/api/stock-prices/")
+			.query({ stock: ["GOOG", "MSFT"], like: "false" })
+			.end((err, res) => {
+				assert.equal(res.status, 200);
+				assert.isObject(res.body);
+				assert.isArray(res.body.stockData);
+                assert.equal(res.body.stockData[0].stock, "GOOG")
+                assert.equal(res.body.stockData[1].stock, "MSFT")
+                assert.property(res.body.stockData[0], 'rel_likes')
+                assert.property(res.body.stockData[1], 'rel_likes')
+				done();
+			});
+	});
+    test("Viewing two stocks and liking them: GET request to /api/stock-prices/", (done) => {
+		chai
+			.request(server)
+			.get("/api/stock-prices/")
+			.query({ stock: ["GOOG", "MSFT"], like: "true" })
+			.end((err, res) => {
+				assert.equal(res.status, 200);
+				assert.isObject(res.body);
+				assert.isArray(res.body.stockData);
+                assert.equal(res.body.stockData[0].stock, "GOOG")
+                assert.equal(res.body.stockData[1].stock, "MSFT")
+                assert.property(res.body.stockData[0], 'rel_likes')
+                assert.property(res.body.stockData[1], 'rel_likes')
+				done();
+			});
+	});
 });
